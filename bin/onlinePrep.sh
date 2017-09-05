f685f94737ef5e338fdab31cdfe395d30fd9e714
root <root@office.domainepublic.net>
2017-09-05 19:25:57 +0200

    custom service port

--- bin/onlinePrep.sh
+++ bin/onlinePrep.sh
@@ -12,6 +12,7 @@ SearchGitOpts=''
 #### Download dependencies ####
 if [ -d ${lool_dir} ]; then
   cd ${lool_dir}
+  git stash
 else
   git clone ${lool_src_repo} ${lool_dir}
   cd ${lool_dir}
@@ -21,6 +22,10 @@ eval "$(SearchGitCommit $SearchGitOpts)"
 if [ -f ${lool_dir}/loolwsd ] && $repChanged ; then
   lool_forcebuild=true
 fi
+# change loolwsd service port
+sed -i -e "s/^\(constexpr int DEFAULT_CLIENT_PORT_NUMBER =\) \([0-9]\+\);$/\1 $loolwsd_service_port;/g" $lool_dir/common/Common.hpp
+# change loolforkit service port
+sed -i -e "s/^\(constexpr int DEFAULT_MASTER_PORT_NUMBER =\) \([0-9]\+\);$/\1 $loolforkit_service_port;/g" $lool_dir/common/Common.hpp
 set +e
 if ! npm -g list jake >/dev/null; then
   npm install -g npm
